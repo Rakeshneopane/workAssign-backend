ce53e867c68e53ec7c669d98f2835ad4f00d3e70
rakeshneopane <rakeshneopane@gmail.com>
2026-03-05 10:26:17 +0530

    Fix: connection to database update

--- dbConnect/dbConnect.js
+++ dbConnect/dbConnect.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoUri = process.env.MONGO_URI;
+let isConnecnted = false;
 
 const initialiseDatabase = async()=>{
+    if (isConnecnted) return;
     try {
-        await mongoose.connect(mongoUri);
-            console.log("Established Connection to Database.")
+        const db = await mongoose.connect(mongoUri);
+        isConnecnted = db.connections[0].readyState;    
+        console.log("Established Connection to Database.")
         
     } catch (error) {
             console.error("Error while connecting.", error.message); 
