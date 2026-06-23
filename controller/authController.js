c46ade2b6074d01c16f8b1e4d2293cbca85f6131
rakeshneopane <rakeshneopane@gmail.com>
2026-06-23 19:00:47 +0530

    fix: frontend url

--- controller/authController.js
+++ controller/authController.js
@@ -162,7 +162,7 @@ exports.getAll = async( req,res,next )=>{
 const isProduction = process.env.NODE_ENV === "production";
 
 const FRONTEND_URL = process.env.NODE_ENV === 'production'
-  ? 'https://work-assign-frontend.vercel.app'
+  ? 'https://work-assign-frontend-6wjt.vercel.app'
   : 'http://localhost:5173';
 
 exports.google = (req,res, next)=>{
